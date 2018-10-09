b21fd465e244f85254dbcb488de8c4cbc3ee9eb8
movahedian <movahedian1369@gmail.com>
2018-10-09 12:08:00 +0330

    #003

--- miningservice/src/main/java/ir/sana24/miningservice/SendEvents.java
+++ miningservice/src/main/java/ir/sana24/miningservice/SendEvents.java
@@ -4,6 +4,15 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.widget.Toast;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -12,6 +21,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class SendEvents {
 
